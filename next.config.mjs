0f0308910ec49493df3a0196067763fd8350962a
Med-iTrack App <meditrack177@gmail.com>
2025-11-14 19:13:07 +0000

    nada el npm run dev me sige diciendo get / 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,12 @@
 /** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
-
-const pwaConfig = withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  sw: 'sw.js',
-});
-
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
-export default pwaConfig(nextConfig);
+export default nextConfig;
