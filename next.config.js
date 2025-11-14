8cfbeff51b56b8f00a7e16b625714e5f08673882
Med-iTrack App <meditrack177@gmail.com>
2025-11-14 18:37:39 +0000

    ¿Puedes darme más detalles sobre el framework de frontend que utilizas e

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
+};
+
+module.exports = withPWA(nextConfig);
